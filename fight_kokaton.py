665e9a1641b05f3c066f9f146e35efbbfeeecf8a
satoukibi <c0a23074b3@edu.yeu.ac.jp>
2024-05-07 15:22:39 +0900

    練習4gameoverこうかとん

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -156,6 +156,11 @@ def main():
         
         if bird.rct.colliderect(bomb.rct):
             # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
+            fonto = pg.font.Font(None, 80)
+            txt = fonto.render("GameOver", True, (255, 0,0))
+            screen.blit(txt, [WIDTH/2-150, HEIGHT/2])
+            pg.display.update()
+            time.sleep(5)
             bird.change_img(8, screen)
             pg.display.update()
             time.sleep(1)
